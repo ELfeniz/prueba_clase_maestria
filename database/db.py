1e267ddf54d2da4aaed961c3a45abf135ed17b68
ELfeniz <juanpavilla1999@gmail.com>
2024-12-07 11:46:03 -0500

    2

--- database/db.py
+++ database/db.py
@@ -11,8 +11,8 @@ This file the connection to the MySQL dataset using sqlalchemy with a local data
 load_dotenv('.env')
 
 # Obtener la URL de conexión desde la variable de entorno
-DATABASE_URL = "postgresql+psycopg2://admin:admin1234%40nn@postgres:5433/Tareas"
-
+DATABASE_URL = "postgresql+psycopg2://admin:admin1234%40nn@20.55.16.254:5433/Tareas"
+ 
 
 # Configuración de SQLAlchemy con PostgreSQL
 engine = create_engine(DATABASE_URL, echo=True)
